9f1d36cde6af9a6d6f83df696623edb2d33f9104
utkarsh vijayvargiya <utkarsh.innoventes@embibe.com>
2021-05-16 10:55:52 +0530

    some changes

---
+++ build/precache-manifest.6fa2d681f2d3837d3fbe62840286fcd4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4d5b529fdaf384b820f294b9a237a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c5627009b323e401e79",
+    "url": "/static/css/2.3d6ebc88.chunk.css"
+  },
+  {
+    "revision": "fa612dcb82f05205c2a3",
+    "url": "/static/css/main.7f471451.chunk.css"
+  },
+  {
+    "revision": "8c5627009b323e401e79",
+    "url": "/static/js/2.1340ca21.chunk.js"
+  },
+  {
+    "revision": "c3317786235eba279c77ed25d5ab8207",
+    "url": "/static/js/2.1340ca21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa612dcb82f05205c2a3",
+    "url": "/static/js/main.86613311.chunk.js"
+  },
+  {
+    "revision": "859c1f9432be821512cd",
+    "url": "/static/js/runtime-main.bf906d1d.js"
+  },
+  {
+    "revision": "59ef1da45f25e6ed92342bf68cbde109",
+    "url": "/static/media/bg-abstract-resized.59ef1da4.png"
+  }
+]);
